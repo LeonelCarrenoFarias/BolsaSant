cfddd19c6ee562fda0fa0289dc2c5c2bd48164e7
LeonelCarrenoFarias <carrenofarias.leonelmatias@gmail.com>
2023-06-15 21:38:18 -0400

    segundo commit

--- consumo.php
+++ consumo.php
@@ -1,17 +1,16 @@
 <?php
 
-$key=$_POST['key'];
-$data =file_get_contents('https://startup.bolsadesantiago.com/api/consulta/TickerOnDemand/getIndices?access_token='.$key.'');
+$key="0051D3CCDE7D4B78A49C0BC6AB28913A";
+$data =file_get_contents('https://startup.bolsadesantiago.com/api/consulta/TickerOnDemand/getIndices?access_token='.$key);
 if($data){
     $data2=json_decode($data,true);
     unlink("tsconfig.json");
     file_put_contents("tsconfig.json", json_encode($data2));
 
-    header('Location: grafico.html');
+    echo "<script>alert('Exito'); window.location.replace('grafico.html');</script>";
 
 }else{
-    echo "<script>alert('Error al cargar la API')</script>";
-    header('Location: index.html');
+    echo "<script>alert('Error al cargar la API'); window.location.replace('index.html');</script>";
 
 }
 
